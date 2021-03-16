04dcea204ea589bb590efcb794d111a6e00966c3
Devang Kamble <risingentropy20@gmail.com>
2021-03-16 17:37:47 +0530

    99 bottles of beer

---
+++ HeadFirstJava/Chapter1/MyFirstApp.java
@@ -0,0 +1,14 @@
+public class MyFirstApp{
+
+
+    //single line comment
+    /* Multiline comment.
+    Super Cool!*/
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello There!");
+        System.out.print("General Kenobi!!");
+    }
+
+}
---
+++ HeadFirstJava/Chapter1/NinetyNineBottlesOfBeer.java
@@ -0,0 +1,30 @@
+public class NinetyNineBottlesOfBeer {
+
+    public static void main(String[] args)
+    {
+        int n = 99;
+        while(n > 0)
+        {
+            if(n==1)
+            {
+                System.out.println(n + " bottle of beer on the wall, " + n + " bottle of beer.");
+            }
+            else
+            {
+                System.out.println(n + " bottles of beer on the wall, " + n + " bottles of beer.");
+            }
+            if(n == 2)
+            {
+                System.out.println("Take one down and pass it around, " + --n +  " bottle of beer.");
+            }
+            else
+            {
+                System.out.println("Take one down and pass it around, " + --n +  " bottles of beer.");
+            }
+        }
+
+
+    }
+
+    
+}
